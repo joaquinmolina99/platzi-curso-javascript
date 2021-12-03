e2cc7ddac96eeb7c5e4e2d20b2ac2272ed8d4ee1
Joaquin Molina <joaquinmolina@MacBook-Air-de-Joaquin.local>
2021-12-03 19:05:22 -0300

    trabajo practico calculando descuentos con cupones

--- descuentos.js
+++ descuentos.js
@@ -1,5 +1,9 @@
-//const precioOriginal = 120;
-//const descuento = 15;
+const cupons = [
+    "joaquin",
+    "molina",
+    "alejandro"
+];
+
 
 function calcularPrecioConDescuento(precio, descuento){
    
@@ -9,23 +13,34 @@ function calcularPrecioConDescuento(precio, descuento){
     return precioConDescuento;
 }
 
-
-//console.log({
-//    precioOriginal,
-//   descuento,
-//    porcentajePrecioConDescuento,
-//    precioConDescuento
-//})
-
 function priceDiscount(){
     const inputPrice = document.getElementById("price")
     const priceValue =  inputPrice.value ;
    
-    const inputDiscount = document.getElementById("discount")
-    const discontValue =  inputDiscount.value ;
+    const inputCupon = document.getElementById("cuponDiscount");
+    const cuponValue = inputCupon.value;
+    
+    let descuento;
 
-    const precioFinal = calcularPrecioConDescuento(priceValue, discontValue);
+    switch(cuponValue) {
+        case cupons[0]: descuento = 15;
+        break;
+        case cupons[1]: descuento = 30;
+        break;
+        case cupons[2]: descuento = 25;
+        break;
+    }
+
+
+    const precioFinal = calcularPrecioConDescuento(priceValue, descuento);
+
 
     const resultPrice = document.getElementById("resultPrice")
+    
+    if (!cupons.includes(cuponValue)) {
+        resultPrice.innerText = "El cupón " + cuponValue + " no es válido";
+    }
+    else
     resultPrice.innerText = "el precio con descuento es $" + precioFinal;
+
 }
